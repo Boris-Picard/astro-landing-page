04724af8270755821a3a0dd6838d82dff9e9b2b3
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 <picard.boris@gmail.com>
2024-07-11 17:56:24 +0200

    feat: removed preline added daisyui

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-    "node_modules/preline/dist/*.js",
-  ],
-  darkMode: "class",
+  content: ["./src/**/*.{astro,html,svelte,vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("preline/plugin")],
+  plugins: [require("daisyui")],
 };
