5b2dc24242281a42cd3d30b2a6baf81ee707c3c7
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 <picard.boris@gmail.com>
2024-07-12 23:08:42 +0200

    feat: trusted section done

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,8 +3,18 @@ export default {
   content: ["./src/**/*.{astro,html,svelte,vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      animation: {
+        scroll: "scroll 20s linear infinite",
+      },
+      keyframes: {
+        scroll: {
+          "0%": { transform: "translateX(10)" },
+          "100%": { transform: "translateX(-101%)" },
+        },
+      },
       backgroundImage: {
-        "radial-gradient-to-top": "radial-gradient(140% 107.13% at 50% 5%, transparent 37.41%, #EF8801 69.27%, #FF6347 100%)",
+        "radial-gradient-to-top":
+          "radial-gradient(140% 107.13% at 50% 5%, transparent 37.41%, #EF8801 69.27%, #FF6347 100%)",
       },
     },
   },
