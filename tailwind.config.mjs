0a293333e3c91306e95f82b64d1efe2dc8760093
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 <picard.boris@gmail.com>
2024-07-12 14:20:07 +0200

    feat: added radial gradient to top

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{astro,html,svelte,vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "radial-gradient-to-top": "radial-gradient(140% 107.13% at 50% 5%, transparent 37.41%, #EF8801 69.27%, #FF6347 100%)",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
