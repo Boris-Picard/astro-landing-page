06d0d759c92dac4d9473c1a85bf8e1dbcfb8bca6
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 <picard.boris@gmail.com>
2024-07-14 00:09:41 +0200

    feat: added scrollup & scrolldown keyframes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,11 +8,21 @@ export default {
       },
       animation: {
         scroll: "scroll 20s linear infinite",
+        scrollUp: "scrollUp 5s linear infinite",
+        scrollDown: "scrollDown 5s linear infinite",
       },
       keyframes: {
         scroll: {
           "0%": { transform: "translateX(0)" },
           "100%": { transform: "translateX(-100%)" },
+        },
+        scrollUp: {
+          "0%": { transform: "translateY(0)" },
+          "100%": { transform: "translateY(-100%)" },
+        },
+        scrollDown: {
+          "0%": { transform: "translateY(0)" },
+          "100%": { transform: "translateY(-100%)" },
         },
       },
     },
