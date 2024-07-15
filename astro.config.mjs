a08cf102f51d5c851c0f154fcbbc74b24dc358fb
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 <picard.boris@gmail.com>
2024-07-15 15:09:23 +0200

    fix: change all img path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/static";
-import svgLoader from "vite-svg-loader"; // Assurez-vous que le plugin est bien importé
 
 export default defineConfig({
   integrations: [tailwind()],
   output: "static",
   adapter: vercel({ imageService: true, devImageService: "squoosh" }),
-  vite: {
-    plugins: [svgLoader()],
-    build: {
-      rollupOptions: {
-        output: {
-          entryFileNames: "entry.[hash].mjs",
-          chunkFileNames: "chunks/chunk.[hash].mjs",
-          assetFileNames: "assets/asset.[hash][extname]",
-        },
-      },
-    },
-  },
 });
