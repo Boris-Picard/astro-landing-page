1c90f20a031f7674e5b1bfe9069f45ed0a59902c
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 <picard.boris@gmail.com>
2024-07-15 12:33:56 +0200

    fix: import svgloader

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/static";
+import svgLoader from "vite-svg-loader";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   vite: {
